7488b21c60a2fab62e9d83761f1f6872c26d5212
y_yamasaki <y_yamasaki@ga-tech.co.jp>
2020-10-25 00:10:18 +0900

    Add: 回答画面作成

---
+++ src/components/Answer.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="answer_row">
+      <span class="answer_rank"> {{rank}} </span>
+      <span class="answer_name"> {{name}} </span>
+      <span class="answer_seconds"> {{seconds}} </span>
+    </div>
+</template>
+
+<script>
+export default {
+name: 'Answer',
+  props: {
+    rank: Number,
+    name: String,
+    seconds: Number
+  }
+}
+</script>
+
+<style lang="scss">
+  .answer_row{
+    height: 10%;
+    background-color: blue;
+    margin: 0.1em 0;
+
+    font-size: 2.5em;
+    color: honeydew;
+    text-shadow: 0.08em 0.1em black;
+    border: 2px solid;
+    border-radius: 0.5em;
+    border-color:white;
+
+    display: flex;
+    justify-content: space-between;
+
+    .answer_rank{
+      display: inline-block;
+      width: 10%;
+      text-align: center;
+      border-radius: 0.5em;
+      background: linear-gradient(#05FBFF, #1E00FF);
+      /* background-color: #05FBFF; */
+    }
+
+    .answer_name{
+      display: inline-block;
+      width: 50%;
+    }
+
+    .answer_seconds{
+      display: inline-block;
+      width: 20%;
+    }
+}
+
+@keyframes AppearAnswer{
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+</style>
---
+++ src/components/Answers.vue
@@ -0,0 +1,74 @@
+<template>
+    <body>
+        <h1 style="color: white;"> オールスター感謝祭 </h1>
+        <div class="answer_all">
+            <div v-for="(AnswerItem, index) in AnswerItemList" v-bind:key="AnswerItem.rank" v-bind:class="'answer-' + index">
+                <Answer :rank="AnswerItem.rank" :name="AnswerItem.name" :seconds="AnswerItem.seconds"/>
+            </div>
+        </div>
+    </body>
+</template>
+
+<script>
+
+import Answer from './Answer.vue'
+export default {
+name: 'Answers',
+  props: {
+    msg: String
+  },
+  components:{
+      Answer
+  },
+  data: function(){
+      return {
+          AnswerItemList: [
+              {rank: 1, name: "Yohei", seconds:1.84},
+              {rank: 2, name: "Yohei", seconds:1.84},
+              {rank: 3, name: "Yohei", seconds:1.84},
+              {rank: 4, name: "Yohei", seconds:1.84},
+              {rank: 5, name: "Yohei", seconds:1.84},
+              {rank: 6, name: "Yohei", seconds:1.84},
+              {rank: 7, name: "Yohei", seconds:1.84},
+              {rank: 8, name: "Yohei", seconds:1.84},
+              {rank: 9, name: "Yohei", seconds:1.84},
+              {rank: 10, name: "Yohei", seconds:1.84},
+          ]
+      }
+  }
+}
+</script>
+
+<style lang="scss">
+
+body {
+    background-image: url("/allstar_high_resolution.png");
+    background-size: cover;
+    background-attachment: fixed;
+    background-position: center center;
+}
+
+.answer_all{
+    width: 50%;
+    margin: auto;
+}
+
+@for $i from 0 through 10 {
+    .answer-#{$i}{
+        opacity: 0;
+        animation-name: AppearAnswer;
+        animation-duration: 0.75s;
+        animation-delay: calc(0.25s *  (10 - #{$i}));
+        animation-fill-mode: forwards; //終了後のスタイルをそのまま使う
+    }
+}
+
+@keyframes AppearAnswer{
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+</style>
